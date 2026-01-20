3f2c1756975b786d61224ecd92241ed921668df7
ValPetrov10110 <valpetrov.soccer@gmail.com>
2026-01-19 23:47:19 -0500

    Reorganized the headers and layed out the foundation for the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,14 @@
-#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <chrono>
+#include <iomanip>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/component/component.hpp>
 #include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
+
+using namespace ftxui;
 
 int main(){
-	using namespace ftxui;
-
-	// Create a simple document with three text elements
-	Element document = hbox({
-		text("left") | border,
-		text("middle") | border | flex,
-		text("right") | border,
-	});
-
-	// Create a screen with full width and height fitting the document
-	auto screen = Screen::Create(
-		Dimension::Full(),		// Width
-		Dimension::Fit(document)	// Height
-	);
-
-	// Render the document onto the screen
-	Render(screen, document);
-	
-	// Print the screen to the console
-	screen.Print();
 
 	return EXIT_SUCCESS;
 }
